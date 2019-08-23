4824b24a3cb7a62f5b639d5d08de858a3b6f90c4
rnl <287325080@qq.com>
2019-08-23 14:57:19 +0800

    no message

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '9.0'
+target 'RYProject' do
+
+# 网络请求
+pod 'AFNetworking'                      , '3.1.0'
+
+end
---
+++ Pods/Target Support Files/Pods-RYProject/Pods-RYProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RYProject : NSObject
+@end
+@implementation PodsDummy_Pods_RYProject
+@end
