ccb47ac1ff558fb05c37307cd65b51aec29a47f1
rnl <287325080@qq.com>
2019-08-23 15:25:28 +0800

    no message

---
+++ RYProject/Class/Helpers/NetHelper/NetWorkHelper.h
@@ -0,0 +1,17 @@
+//
+//  NetWorkHelper.h
+//  RYProject
+//
+//  Created by rnl on 2019/8/23.
+//  Copyright © 2019 rnl. All rights reserved.
+//
+
+#import "AFHTTPSessionManager.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NetWorkHelper : AFHTTPSessionManager
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RYProject/Class/Helpers/NetHelper/NetWorkHelper.m
@@ -0,0 +1,13 @@
+//
+//  NetWorkHelper.m
+//  RYProject
+//
+//  Created by rnl on 2019/8/23.
+//  Copyright © 2019 rnl. All rights reserved.
+//
+
+#import "NetWorkHelper.h"
+
+@implementation NetWorkHelper
+
+@end
