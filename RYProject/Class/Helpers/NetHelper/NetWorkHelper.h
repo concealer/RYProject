d919e0a1167e17933e4e0ecfa3cb58e20182ea4f
rnl <287325080@qq.com>
2019-08-23 15:35:30 +0800

    no message

--- RYProject/Class/Helpers/NetHelper/NetWorkHelper.h
+++ RYProject/Class/Helpers/NetHelper/NetWorkHelper.h
@@ -8,6 +8,16 @@
 
 #import "AFHTTPSessionManager.h"
 
+typedef NS_ENUM(NSUInteger,NetWorkHTTPMethod) {
+    GET = 0,
+    POST,
+};
+
+typedef NS_ENUM(NSUInteger,NetWorkQNImageMethod) {
+    PHASSET = 0,
+    DATA,
+};
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface NetWorkHelper : AFHTTPSessionManager
